b84c22ee747e5009dac30c26e9edb2e6817d84ad
Techiral <115097129+Techiral@users.noreply.github.com>
2025-06-07 11:09:31 +0530

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,7 +111,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en\" suppressHydrationWarning>
       <head>
         <script
           type="application/ld+json"
